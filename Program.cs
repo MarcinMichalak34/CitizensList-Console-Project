260e8e6f528c241e190749012f54f34c02488d15
Marcin <108423829+MarcinMichalak34@users.noreply.github.com>
2023-01-08 16:41:44 +0100

    overwriting not working

--- Program.cs
+++ Program.cs
@@ -1,15 +1,43 @@
-﻿using System;
-using CitizensList;
+﻿using CitizensList;
+using System;
 
 List<Person> citizensList = new();
 
 var fileName = "CitizenList.txt";
 
-Console.Write("1. City: ");
-var city = Console.ReadLine();
-Console.Write("2. Name: ");
-var name = Console.ReadLine();
-Console.Write("3. Last Name: ");
-var lastName = Console.ReadLine();
-Console.Write("4. Personal ID Number: ");
-var personalIDNumber = Console.ReadLine();
+using (StreamWriter sw = new(fileName))
+{
+    do
+    {
+        Console.Write("1. City: ");
+        var city = Console.ReadLine();
+        Console.Write("2. Name: ");
+        var name = Console.ReadLine();
+        Console.Write("3. Last Name: ");
+        var lastName = Console.ReadLine();
+        Console.Write("4. Personal ID Number: ");
+        var personalIDNumber = Console.ReadLine();
+        if (!Helper.PersonalIDNumberVerification(personalIDNumber))
+        {
+            Console.WriteLine("Wrong Personal ID Number, please try again.");
+        }
+        else
+        {
+            var currentCitizen = citizensList.FirstOrDefault(x => x.PersonalIDNumber == personalIDNumber);
+            if (currentCitizen != null)
+            {
+                Console.WriteLine("Citizen exists, overwriting data.");
+                Person.PersonDataUpdate(currentCitizen, name, lastName);
+                break;
+            }
+            else
+            {
+                var citizen = new Person(name, lastName, personalIDNumber, city);
+                citizensList.Add(citizen);
+                Console.WriteLine("New citizen add to database.");
+                sw.WriteLine(Person.PersonToString(citizen));
+                Console.WriteLine("If you want add another citizen, click ENTER. If you want exit program, click ESC.");
+            }
+        }
+    } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+}
