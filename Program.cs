3a0fee23f53d4bbd1367c0dd1f6d1a7edbd29916
Marcin <108423829+MarcinMichalak34@users.noreply.github.com>
2023-01-08 16:31:59 +0100

    code without create file function and overwriting function

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using CitizensList;
+
+List<Person> citizensList = new();
+
+var fileName = "CitizenList.txt";
+
+Console.Write("1. City: ");
+var city = Console.ReadLine();
+Console.Write("2. Name: ");
+var name = Console.ReadLine();
+Console.Write("3. Last Name: ");
+var lastName = Console.ReadLine();
+Console.Write("4. Personal ID Number: ");
+var personalIDNumber = Console.ReadLine();
