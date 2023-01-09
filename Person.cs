69a9df470e890ce8cab8c9de6ba91896793f9cad
Marcin <108423829+MarcinMichalak34@users.noreply.github.com>
2023-01-09 16:29:45 +0100

    Add files via upload

---
+++ Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CitizensList
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string PersonalIDNumber { get; set; }
+        public string City { get; set; }
+
+        public Person(string name, string lastName, string personalIDNumber, string city)
+        {
+            Name = name;
+            LastName = lastName;
+            PersonalIDNumber = personalIDNumber;
+            City = city;
+        }
+
+        public static void PersonDataUpdate(Person citizen, string newName, string newLastName)
+        {
+            citizen.Name = newName;
+            citizen.LastName = newLastName;
+        }
+
+        public static string PersonToString(Person citizen)
+        {
+            string text = $"City: {citizen.City} \n Name: {citizen.Name} \n Last Name: {citizen.LastName} \n Personal ID Number: {citizen.PersonalIDNumber}";
+            return text;
+        }
+    }
+}
