1742aa57e267e48791c17f0e82fce39271bfb77e
Marcin <108423829+MarcinMichalak34@users.noreply.github.com>
2023-01-09 16:06:30 +0100

    Overwriting working

--- Program.cs
+++ Program.cs
@@ -1,17 +1,14 @@
 ﻿using CitizensList;
 using System;
 
-List<Person> citizensList = new();
-
 var fileName = "CitizenList.txt";
 
-using (StreamWriter sw = new(fileName))
 {
     do
     {
         Console.Write("1. City: ");
-        var city = Console.ReadLine();
-        Console.Write("2. Name: ");
+        var city =  Console.ReadLine();
+        Console.Write("2. Name: "); 
         var name = Console.ReadLine();
         Console.Write("3. Last Name: ");
         var lastName = Console.ReadLine();
@@ -23,21 +20,28 @@ using (StreamWriter sw = new(fileName))
         }
         else
         {
-            var currentCitizen = citizensList.FirstOrDefault(x => x.PersonalIDNumber == personalIDNumber);
+            var currentCitizen = Person.CitizensList.FirstOrDefault(x => x.PersonalIDNumber == personalIDNumber);
             if (currentCitizen != null)
             {
                 Console.WriteLine("Citizen exists, overwriting data.");
                 Person.PersonDataUpdate(currentCitizen, name, lastName);
-                break;
             }
             else
             {
                 var citizen = new Person(name, lastName, personalIDNumber, city);
-                citizensList.Add(citizen);
+                Person.CitizensList.Add(citizen);
                 Console.WriteLine("New citizen add to database.");
-                sw.WriteLine(Person.PersonToString(citizen));
                 Console.WriteLine("If you want add another citizen, click ENTER. If you want exit program, click ESC.");
             }
+            using (StreamWriter sw = new(fileName))
+            { 
+                foreach (var citizen in Person.CitizensList)
+                {
+                    sw.WriteLine(Person.PersonToString(citizen));
+                }
+            }
         }
+
     } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
 }
+
